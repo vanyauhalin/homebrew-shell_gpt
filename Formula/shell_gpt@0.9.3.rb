b86757107c0e88cb3fac8e9c23ea5dc217ff2588
vanyauhalin <vanyauhalin@gmail.com>
2023-06-14 20:26:54 +0400

    Fix sha256

--- Formula/shell_gpt@0.9.3.rb
+++ Formula/shell_gpt@0.9.3.rb
@@ -4,7 +4,7 @@ class ShellGptAT093 < Formula
   desc "A command-line productivity tool powered by GPT-3 and GPT-4, will help you accomplish your tasks faster and more efficiently"
   homepage "https://github.com/TheR1D/shell_gpt"
   url "https://github.com/TheR1D/shell_gpt/archive/refs/tags/0.9.3.tar.gz"
-  sha256 "83bcd0f628b7f2304c513d27f128e471c9c6964a423b8f35ec6676ca2771a4c5"
+  sha256 "cdd8545a79cdd706e199e41b0aa119a829c9a687331762e69ea95946d3d729a3"
   license "MIT"
   head "https://github.com/TheR1D/shell_gpt", branch: "main"
 
